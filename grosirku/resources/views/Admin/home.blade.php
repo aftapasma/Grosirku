f24f2975f5d4db15eaf50229a861b8447ff40697
lenovo <aftaplasma708@gmail.com>
2023-12-31 12:43:37 +0700

    add sidenavbar

--- grosirku/resources/views/Admin/home.blade.php
+++ grosirku/resources/views/Admin/home.blade.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Nucleo Icons -->
+    <link href="../resources/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../resources/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Popper -->
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <!-- Main Styling -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-<h1>admin asu</h1>
+<body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+    <x-sidenav />
 </body>
 </html>
