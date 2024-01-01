4db3cbdfce6bc0a504b67176c51e62b401ceed29
lantibisl <114908081+lantibisl@users.noreply.github.com>
2024-01-01 12:41:34 +0700

    add search functionality

--- grosirku/resources/views/customer/home.blade.php
+++ grosirku/resources/views/customer/home.blade.php
@@ -42,33 +42,33 @@
         <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">Kategori</h2>
         <div class="grid grid-cols-3 gap-3">
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-1.jpg" alt="category 1" class="w-full">
+                <img src="{{asset('storage/images/logo_grosirku.png')}}" alt="category 1" class="w-full">
                 <a href="#"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Gula pasir</a>
             </div>
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-2.jpg" alt="category 1" class="w-full">
+                <img src="{{asset('storage/images/logo_grosirku.png')}}" alt="category 1" class="w-full">
                 <a href="#"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Minyak goreng</a>
             </div>
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-3.jpg" alt="category 1" class="w-full">
+                <img src="{{asset('storage/images/logo_grosirku.png')}}" alt="category 1" class="w-full">
                 <a href="#"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Tepung
                 </a>
             </div>
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-4.jpg" alt="category 1" class="w-full">
+                <img src="{{asset('storage/images/logo_grosirku.png')}}" alt="category 1" class="w-full">
                 <a href="#"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Beras</a>
             </div>
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-5.jpg" alt="category 1" class="w-full">
+                <img src="{{asset('storage/images/logo_grosirku.png')}}" alt="category 1" class="w-full">
                 <a href="#"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Mie Instan</a>
             </div>
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-6.jpg" alt="category 1" class="w-full">
+                <img src="{{asset('storage/images/logo_grosirku.png')}}" alt="category 1" class="w-full">
                 <a href="#"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Makanan Ringan</a>
             </div>
