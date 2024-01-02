b1137b3a964bf9a1876ce4370cb8a276bf265337
lantibisl <114908081+lantibisl@users.noreply.github.com>
2024-01-02 15:47:27 +0700

    fix admin dashboard ui

--- grosirku/resources/views/Admin/home.blade.php
+++ grosirku/resources/views/Admin/home.blade.php
@@ -51,7 +51,10 @@
                      <p class="block antialiased font-sans text-sm text-blue-gray-900 font-normal leading-none opacity-70">Status</p>
                   </th>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                     <p class="block antialiased font-sans text-sm text-blue-gray-900 font-normal leading-none opacity-70">Total</p>
+                    <p class="block antialiased font-sans text-sm text-blue-gray-900 font-normal leading-none opacity-70">Jumlah Produk</p>
+                 </th>
+                  <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
+                     <p class="block antialiased font-sans text-sm text-blue-gray-900 font-normal leading-none opacity-70">Total Harga</p>
                   </th>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
                      <p class="block antialiased font-sans text-sm text-blue-gray-900 font-normal leading-none opacity-70"></p>
@@ -59,96 +62,35 @@
                </tr>
                </thead>
                <tbody>
+                 @foreach($transactions as $transaction)
+                    @php
+                       $product = \App\Models\Product::find($transaction->product_id);
+                       $user = \App\Models\User::find($transaction->user_id);
+                    @endphp
                <tr>
                   <td class="p-4 border-b border-blue-gray-50">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">1</p>
+                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{{$user->id}}</p>
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
                      <div class="flex items-center gap-3">
-                     <img src="https://docs.material-tailwind.com/img/logos/logo-spotify.svg" alt="Spotify" class="inline-block relative object-center !rounded-full w-12 h-12 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Spotify</p>
-                     </div>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <div class="w-max">
-                     <div class="relative grid items-center font-sans font-bold uppercase whitespace-nowrap select-none bg-green-500/20 text-green-900 py-1 px-2 text-xs rounded-md" style="opacity: 1;">
-                        <span class="">selesai</span>
-                     </div>
-                     </div>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">$1</p>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <button class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button">
-                     <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-4 w-4">
-                           <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"></path>
-                        </svg>
-                     </span>
-                     </button>
-                  </td>
-               </tr>
-               <tr>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">1</p>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <div class="flex items-center gap-3">
-                     <img src="https://docs.material-tailwind.com/img/logos/logo-spotify.svg" alt="Spotify" class="inline-block relative object-center !rounded-full w-12 h-12 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Spotify</p>
-                     </div>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <div class="w-max">
-                     <div class="relative grid items-center font-sans font-bold uppercase whitespace-nowrap select-none bg-amber-500/20 text-amber-900 py-1 px-2 text-xs rounded-md" style="opacity: 1;">
-                        <span class="">pending</span>
-                     </div>
-                     </div>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">$1</p>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <button class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button">
-                     <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-4 w-4">
-                           <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"></path>
-                        </svg>
-                     </span>
-                     </button>
-                  </td>
-               </tr>
-               <tr>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">1</p>
-                  </td>
-                  <td class="p-4 border-b border-blue-gray-50">
-                     <div class="flex items-center gap-3">
-                     <img src="https://docs.material-tailwind.com/img/logos/logo-spotify.svg" alt="Spotify" class="inline-block relative object-center !rounded-full w-12 h-12 rounded-lg border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">Spotify</p>
+                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">{{$user->name}}</p>
                      </div>
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
                      <div class="w-max">
                      <div class="relative grid items-center font-sans font-bold uppercase whitespace-nowrap select-none bg-red-500/20 text-red-900 py-1 px-2 text-xs rounded-md" style="opacity: 1;">
-                        <span class="">cancelled</span>
+                        <span class="">{{$transaction->status}}</span>
                      </div>
                      </div>
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
-                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">$1</p>
-                  </td>
+                    <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">{{$transaction->quantity}}</p>
+                 </td>
                   <td class="p-4 border-b border-blue-gray-50">
-                     <button class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20" type="button">
-                     <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-4 w-4">
-                           <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"></path>
-                        </svg>
-                     </span>
-                     </button>
+                     <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">Rp {{$transaction->total_price}}</p>
                   </td>
                </tr>
+               @endforeach
                </tbody>
             </table>
          </div>
@@ -162,41 +104,48 @@
             <div class="inline-block min-w-full align-middle">
                <div class="overflow-hidden ">
                   <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                        <thead class="bg-gray-100 dark:bg-gray-700">
-                           <tr>
-                              <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                    FOTO
-                              </th>
-                              <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                    Product Name
-                              </th>
-                              <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                    Total
-                              </th>
-                              <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                    Harga
-                              </th>
-                              <th scope="col" class="p-4">
-                                    <span class="sr-only">Edit</span>
-                              </th>
-                           </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                           <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                              <td class="p-4 w-4">
-                                    <div class="flex items-center">
-                                    <img class="h-auto max-w-lg rounded-lg" src="" alt="barang">
-                                    </div>
-                              </td>
-                              <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple Imac 27"</td>
-                              <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">12</td>
-                              <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">RP1999</td>
-                              <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-                                    <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                              </td>
-                           </tr>
-                        </tbody>
-                  </table>
+                     <thead class="bg-gray-100 dark:bg-gray-700">
+                        <tr>
+                           <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                 FOTO
+                           </th>
+                           <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                 Nama Produk
+                           </th>
+                           <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                 Kategori
+                           </th>
+                           <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                 Harga
+                           </th>
+                           <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                 Stok
+                           </th>
+                           <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                 Terjual
+                           </th>
+                       </th>
+                        </tr>
+                     </thead>
+                     <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                       @foreach ($products as $index => $product)
+                           @if ($index < 5)
+                        <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">  
+                             <td class="p-4 w-4">
+                                 <div class="flex items-center">
+                                 <img class=" max-w-lg rounded-lg relative inline-block h-12 w-12" src="{{$product->image ? asset('storage/' . $product->image) : asset('storage/images/logo_grosirku.png')}}" alt="barang">
+                                 </div>
+                           </td>
+                           <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$product->name}}</td>
+                           <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">{{$product->category}}</td>
+                           <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$product->price}}</td>
+                           <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$product->stock}}</td>
+                           <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$product->sold}}</td>
+                     </tr>
+                        @endif
+                     @endforeach
+                     </tbody>
+               </table>
                </div>
             </div>
       </div>
